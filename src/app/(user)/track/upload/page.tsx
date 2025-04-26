a901b6287cdebdf749770764545c723e4cb52dde
Le Quang Trong Nghia <lqtnghia2602@gmail.com>
2025-04-26 22:23:20 +0700

    tích hợp Drag/Drop

---
+++ src/app/(user)/track/upload/page.tsx
@@ -0,0 +1,13 @@
+import UploadTabs from "@/components/track/upload.tabs";
+import { Container } from "@mui/material";
+import React from "react";
+
+const UploadPage = () => {
+  return (
+    <Container>
+      <UploadTabs />
+    </Container>
+  );
+};
+
+export default UploadPage;
